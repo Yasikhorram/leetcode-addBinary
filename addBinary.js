b8b6e73572d7b50596aab6437a5d16ebcb385d13
Yasikhorram <khorramyasaman@gmail.com>
2022-04-03 21:53:01 -0500

    create a loop with couple conditions

--- addBinary.js
+++ addBinary.js
@@ -3,5 +3,10 @@ function addBinary(a, b) {
   let aArr = a.split("").reverse();
   let bArr = b.split("").reverse();
   let carryOver = 0;
+  for (
+    let i = 0, j = 0;
+    i < aArr.length || j < bArr.length || carryOver > 0;
+    i++, j++
+  ) {}
   return resultArr.reverse().join("");
 }
